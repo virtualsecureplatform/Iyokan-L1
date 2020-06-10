02f9eb30dabefca5f201c9bf4c1a1477742c9a68
Naoki Matsumoto <m.naoki9911@gmail.com>
2020-06-10 23:51:04 +0900

    Handle array containing not integer value

--- Models/YosysNetname.cs
+++ Models/YosysNetname.cs
@@ -5,7 +5,7 @@ namespace Iyokan_L1.Models
     public class YosysNetname
     {
         public string hide_name { get; set; }
-        public List<int> bits { get; set; }
+        public List<string> bits { get; set; }
         public Dictionary<string, string> attributes { get; set; }
     }
 }
